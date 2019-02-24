d80dacf79c2971314a1bbb975f46609732241a0e
Amerlaceset <46226856+Amerlaceset@users.noreply.github.com>
2019-02-24 17:51:03 +0100

    Create moni.sh

---
+++ moni.sh
@@ -0,0 +1,35 @@
+#colors
+g='\033[1;32m'
+p='\033[1;35m'
+cyan='\033[1;36m'
+green='\033[1;32m'
+red='\033[1;31m'
+yellow='\033[1;33m'
+blue='\033[1;34m'
+purple='\033[1;35m'
+reset='\033[0m'
+y='\033[1;33m'
+n=install
+cd
+
+cd $HOME/payload6/.tool
+
+echo -e "$red "
+
+figlet -f big "payload6"
+
+echo -e " $cyan "
+
+figlet -f big "Amerr "
+echo -e " $blue ▇◤▔▔▔▔▔▔▔◥▇"
+echo -e " $blue ▇▏◥▇◣┊◢▇◤▕▇"
+echo -e " $blue ▇▏▃▆▅▎▅▆▃▕▇"
+echo -e " $blue ▇▏╱▔▕▎▔▔╲▕▇"                                                                                            echo -e " $blue ▇◣◣▃▅▎▅▃◢◢▇"
+echo -e " $blue ▇▇◣◥▅▅▅◤◢▇▇"
+echo -e " $blue ▇▇▇◣╲▇╱◢▇▇▇"
+echo -e " $blue ▇▇▇▇◣▇◢▇▇▇▇"
+echo -e "$green"
+
+figlet -f big " Ahla bik ^_^ "
+echo -e " تم التحديث الى اخر"
+echo -e " عند التحديث سوف اخبركم "
