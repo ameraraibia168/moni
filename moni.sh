172f2640cc5a82aeb1e9f7e13016d4aa6c4e9021
Amerlaceset <46226856+Amerlaceset@users.noreply.github.com>
2019-02-26 16:02:06 +0100

    Create moni.sh

---
+++ moni.sh
@@ -0,0 +1,50 @@
+#colors
+g='\033[1;32m'
+p='\033[1;35m'
+cyan='\033[1;36m'
+green='\033[1;32m'
+red='\033[1;31m'
+yellow='\033[1;33m'
+blue='\033[1;34m'
+purple='\033[1;35m'
+reset='\033[0m'
+y='\033[1;33m'
+n=install
+cd
+
+cd $HOME/payload6/.tool
+
+echo -e "$red "
+
+figlet -f big "payload6"
+
+echo -e " $cyan "
+
+figlet -f big "Amerr DZ "
+echo -e " $blue ▇◤▔▔▔▔▔▔▔◥▇ $green ╲╲╲╲╲┏━━━━┓╱╱╱╱╱$red ╭╮╮╱▔▔▔▔╲╭╭╮"
+echo -e " $blue ▇▏◥▇◣┊◢▇◤▕▇ $green ╭┓┏╮┏┫╰╯╰╯┣┓╭┓┏╮$red ╰╲╲▏▂╲╱▂▕╱╱╯"
+echo -e " $blue ▇▏▃▆▅▎▅▆▃▕▇ $green ┃┗┛┃┗┫┗━━┛┣┛┃┗┛┃$red ┈┈╲▏▇▏▕▇▕╱┈┈"
+echo -e " $blue ▇▏╱▔▕▎▔▔╲▕▇ $green ╰┳┳╯┏┻━━━━┻┓╰┳┳╯$red ┈┈╱╲▔▕▍▔╱╲┈┈"                            
+echo -e " $blue ▇▇◣◥▅▅▅◤◢▇▇ $green ╱┣┗┳┫┈▕╲▂╱▏┣┳┛┫╲$red ╭╱╱▕╋╋╋╋▏╲╲╮"
+echo -e " $blue ▇▇▇◣╲▇╱◢▇▇▇ $green ╱╰┻┻┫┈▕▕♡▏▏┣┻┻╯╲$red ╰╯╯┈╲▂▂╱┈╰╰╯"
+echo -e " $blue ▇▇▇▇◣▇◢▇▇▇▇ $green ╱╱╱╱┃┈▕╱▔╲▏┃╲╲╲╲"
+
+
+#echo -e "$green"
+echo -e "$cyan"
+echo -e "|---{$yellow my ip$cyan }---| "
+       curl ifconfig.me
+echo -e "$blue"
+
+ifconfig wlan0 | grep -o 192..........
+#echo -e "$green "
+
+
+echo -e ""
+echo -e "$red $===========================================||'"
+
+echo -e "$blue   ***### رادصا رخا  ###*** "
+echo -e "$green  ### مكربخا فوس ةادالا تثدح اذا ###"
+echo -e "$blue   ***### Latest version ###***"
+echo -e "$green  ### When updating the tool will tell you ###"
+echo -e "$red $===========================================||'"
