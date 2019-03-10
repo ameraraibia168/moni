e6fedc631063dde892b607b055704afcb19a4214
Amerlaceset <46226856+Amerlaceset@users.noreply.github.com>
2019-03-10 01:05:53 +0100

    Create moni.sh

---
+++ moni.sh
@@ -0,0 +1,42 @@
+#colors
+g='\033[1;32m'
+p='\033[1;35m'
+cyan='\033[1;36m'
+green='\033[1;32m'
+red='\033[1;31m'
+yellow='\033[1;33m'
+blue='\033[1;34m'
+purple='\033[1;35m'
+reset='\033[0m'
+y='\033[1;33m'
+n=install
+cd
+
+rm -rf payload7
+rm -rf payload8
+cd $HOME/Virus4/.tools
+
+echo -e "$red "
+
+figlet -f big "Amerr Dz"
+
+echo -e " $cyan "
+
+figlet -f big "Virus 4 "
+echo -e " $blue ▇◤▔▔▔▔▔▔▔◥▇ $green ╲╲╲╲╲┏━━━━┓╱╱╱╱╱$red ╭╮╮╱▔▔▔▔╲╭╭╮"
+echo -e " $blue ▇▏◥▇◣┊◢▇◤▕▇ $green ╭┓┏╮┏┫╰╯╰╯┣┓╭┓┏╮$red ╰╲╲▏▂╲╱▂▕╱╱╯"
+echo -e " $blue ▇▏▃▆▅▎▅▆▃▕▇ $green ┃┗┛┃┗┫┗━━┛┣┛┃┗┛┃$red ┈┈╲▏▇▏▕▇▕╱┈┈"
+echo -e " $blue ▇▏╱▔▕▎▔▔╲▕▇ $green ╰┳┳╯┏┻━━━━┻┓╰┳┳╯$red ┈┈╱╲▔▕▍▔╱╲┈┈"$
+echo -e " $blue ▇▇◣◥▅▅▅◤◢▇▇ $green ╱┣┗┳┫┈▕╲▂╱▏┣┳┛┫╲$red ╭╱╱▕╋╋╋╋▏╲╲╮"
+echo -e " $blue ▇▇▇◣╲▇╱◢▇▇▇ $green ╱╰┻┻┫┈▕▕♡▏▏┣┻┻╯╲$red ╰╯╯┈╲▂▂╱┈╰╰╯"
+echo -e " $blue ▇▇▇▇◣▇◢▇▇▇▇ $green ╱╱╱╱┃┈▕╱▔╲▏┃╲╲╲╲"
+
+echo -e "$red $===========================================||'"
+
+echo -e "$blue   ***###رادصا رخا###***"
+echo -e "$green  ### مكربخا فوس ةادالا ثيدحت دنع###"
+echo -e "$blue   ***### Latest version###***"
+echo -e "$green  ### When updating the tool will tell you###"
+echo -e "$red $===========================================||'"
+
+echo -e " $green Virus4 اهتيمست ةادعاو payload6 ةادا فذح متسي"
