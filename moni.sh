42d6984e7433185935ecc82c105994a24e07e01a
Amerlaceset <46226856+Amerlaceset@users.noreply.github.com>
2019-03-24 07:32:23 +0100

    Add files via upload

---
+++ moni.sh
@@ -0,0 +1,53 @@
+#colors
+g='\033[1;32m'
+p='\033[1;35m'
+cyan='\033[1;36m'
+green='\033[1;32m'
+red='\033[1;31m'
+yellow='\033[1;33m'
+blue='\033[1;34m'
+purple='\033[1;35m'
+reset='\033[0m'
+y='\033[1;33m'
+n=install
+sleep 0.01
+cd
+clear
+sleep 0.01
+
+python $HOME/moni/.mona.py
+
+cd $HOME/Virus4/.tools
+sleep 0.01
+echo -e "$red "
+sleep 0.01
+figlet -f big "I LOVE YOU "
+sleep 0.01
+echo -e " $blue"
+sleep 0.01
+figlet -f big "Virus 4 "
+sleep 0.01
+echo -e "                      $blue V $green i $reset r $purple u $cyan s $yellow 4 $green ^_^"
+sleep 0.01
+echo -e " $blue ▇◤▔▔▔▔▔▔▔◥▇ $green ╲╲╲╲╲┏━━━━┓╱╱╱╱╱ $purple┊┊┊▕▔▔▔▔▏┊┊┊Amer┊$red ╭╮╮╱▔▔▔▔╲╭╭╮"
+sleep 0.01
+echo -e " $blue ▇▏◥▇◣┊◢▇◤▕▇ $green ╭┓┏╮┏┫╰╯╰╯┣┓╭┓┏╮ $purple┊┊▂▂▇▇▇▇▂▂┊┊┊DZ┊┊$red ╰╲╲▏▂╲╱▂▕╱╱╯"
+sleep 0.01
+echo -e " $blue ▇▏▃▆▅▎▅▆▃▕▇ $green ┃┗┛┃┗┫┗━━┛┣┛┃┗┛┃ $purple┊▕▂▂▂▂▂▂▂▂▏Amerr $red ┈┈╲▏▇▏▕▇▕╱┈┈"
+sleep 0.01
+echo -e " $blue ▇▏╱▔▕▎▔▔╲▕▇ $green ╰┳┳╯┏┻━━━━┻┓╰┳┳╯ $purple┊┊┏┫╭▅╭▅┣┓┊┊╭╯-- $red ┈┈╱╲▔▕▍▔╱╲┈┈"
+sleep 0.01
+echo -e " $blue ▇▇◣◥▅▅▅◤◢▇▇ $green ╱┣┗┳┫┈▕╲▂╱▏┣┳┛┫╲ $purple┊┊╰╮▔╭╮▔╭╯┊╭╯┊┊┊ $red ╭╱╱▕╋╋╋╋▏╲╲╮"
+sleep 0.01
+echo -e " $blue ▇▇▇◣╲▇╱◢▇▇▇ $green ╱╰┻┻┫┈▕▕♡▏▏┣┻┻╯╲ $purple┊┊┊╯╰━━━▃▃▃▇┊┊┊┊ $red ╰╯╯┈╲▂▂╱┈╰╰╯"
+sleep 0.01
+echo -e " $blue ▇▇▇▇◣▇◢▇▇▇▇ $green ╱╱╱╱┃┈▕╱▔╲▏┃╲╲╲╲ $purple╱▔▔╯╯╯╰╰╰▔▔╲┊┊┊┊ $cyan  v{ 3.9.9 }"
+
+sleep 0.01
+
+echo -e "$red .||====$red>$blue>>>$red>$green>>>$red>$blue>>$red>$green>>$red>── $green Amer.Amerr  $red──<$green<<$red<$blue<<$red<$green<<<$red<$blue<<<$red<====||'"
+
+sleep 0.01
+
+echo -e "                      $blue V $green i $reset r $purple u $cyan s $yellow 4 $green ^_^"
+sleep 0.01
